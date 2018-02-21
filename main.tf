8fdaa9b9783313def2021ec1654f0ac60cedc319
root <root@touchstone4.mylabserver.com>
2018-02-21 14:52:42 +0000

    Adding security groups

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
   	      #!/bin/bash
 	      echo "Hello, World" > index.html
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 aws_profile = "superhero"
 aws_region  = "us-east-1"
+server_port = "8080"
