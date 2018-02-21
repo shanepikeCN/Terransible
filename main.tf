722539f29f65ffc6cc84cdbcb12a6429f8529920
root <root@touchstone4.mylabserver.com>
2018-02-21 13:38:13 +0000

    Adding variables

--- main.tf
+++ main.tf
@@ -8,7 +8,25 @@ resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+  	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p "${var.server_port}" &
+	      EOF
+
+
   tags {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+	from_port = "${var.server_port}"
+	to_port = "${var.server_port}"
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,5 @@
 variable "aws_region" {}
 variable "aws_profile" {}
+variable "server_port" {
+	description = "The port the servers will use for HTTP requests"
+}
