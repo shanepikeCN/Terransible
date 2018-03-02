9778e685ece619f833b123aefdcd799ee0036ff7
root <root@touchstone4.mylabserver.com>
2018-03-02 15:07:44 +0000

    Adding deployer key

--- main.tf
+++ main.tf
@@ -10,18 +10,16 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSfaPktvPwiMmWPLtjMu9hjiYzHgE1lP0a995M6E86yGkyxnFe7ZAksaD+nNm2awUeYK1I1txGSzW878yCamypto3Z1458o1tfIc9Lsp5MUfsBKKqpYn8jYqCXIxZAmHPhnZWIGY5i7sX99/oFR9zZpISNWJLQ6e///PbfmNMkAwW/iqy0AdZdXHPn7jzj3skn0KY2UI1Hb9viFfStTWvkkZ/Y3xACjKTUTtfMar/4lmRhjWv1BXi/ZJ8jR5sRcN/c4YUgE7J93d756joivoOeny2MB/pl+h6VLR1HBaAL8L/11/ZWC7FanUL7W4eTF/ePHih+uuk0bs85uTXxzxCN aws_terraform_ssh_key"
 }
 
+data "template_file" "user_data" {
+  template = "${file("bootstrap.sh")}"
+}
 
 resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  key_name = "${var.key_pair}"
-  user_data = <<-EOF
-  	      #!/bin/bash
-	      echo "Hello, World" > index.html
-	      nohup busybox httpd -f -p "${var.server_port}" &
-	      EOF
-
+  user_data = "${data.template_file.user_data.rendered}"
+  key_name = "deployer-key"
 
   tags {
     Name = "terraform-example"
