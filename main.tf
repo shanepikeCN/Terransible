afcf0798bafa0d78cd69cbdc81a98ec584b5e436
root <root@touchstone4.mylabserver.com>
2018-03-02 16:17:00 +0000

    Modifying ELB SG

--- main.tf
+++ main.tf
@@ -42,6 +42,27 @@ resource "aws_security_group" "instance" {
   }
 }
 
+# Security group for load balancer
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  # Allow outgoing traffic
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow incoming traffic to port 80
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Creating launch configuration to be used for auto scaling groups
 resource "aws_launch_configuration" "example" {
   image_id = "ami-2d39803a"
@@ -58,6 +79,12 @@ resource "aws_autoscaling_group" "example" {
   availability_zones = ["${data.aws_availability_zones.all.names}"]
   min_size = 2
   max_size = 2
+
+  # Register instances with ASG
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+
   tag {
     key = "Name"
     value = "terraform-asg-example"
@@ -68,7 +95,20 @@ resource "aws_autoscaling_group" "example" {
 # Adding load balancer for EC2 instances
 resource "aws_elb" "example" {
   name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+ 
+  # Health check will check port 8080 on EC2 instances every 30 seconds 
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+
+  
+  # LB will listen on port 80 and direct traffic to EC2 instances
   listener {
     lb_port = 80
     lb_protocol = "http"
@@ -93,4 +133,8 @@ resource "aws_elb" "example" {
 
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
