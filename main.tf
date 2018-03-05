b7a0c8a608ec64ffa0fb8409b34c59d3e8fa9732
root <root@touchstone4.mylabserver.com>
2018-03-05 10:35:11 +0000

    Adding IAM resources

--- main.tf
+++ main.tf
@@ -14,7 +14,56 @@ resource "aws_key_pair" "deployer" {
 # Get list of availability zones
 data "aws_availability_zones" "all" {}
 
+#S3_access
 
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name = "s3_access"
+  role = "${aws_iam_role.s3_access_role.name}"
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+  },
+      "Effect": "Allow",
+      "Sid": ""
+      }
+    ]
+}
+EOF
+}
+
+
+
+
+
+/*
 # Runs bootstrap shell script when EC2 instance is created
 data "template_file" "user_data" {
   template = "${file("bootstrap.sh")}"
@@ -138,3 +187,4 @@ resource "aws_elb" "example" {
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
 }
+*/
