66e33a2242329c663c36e834b430732c3bbd4526
root <root@touchstone4.mylabserver.com>
2018-03-05 10:25:33 +0000

    New update

--- main.tf
+++ main.tf
@@ -131,9 +131,9 @@ resource "aws_elb" "example" {
 
 
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
-}
+#output "public_ip" {
+#  value = "${aws_instance.example.public_ip}"
+#}
 
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
