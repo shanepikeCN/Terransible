a1b189e84ad091918f48ba91500b6cfb3b9660c9
root <root@touchstone4.mylabserver.com>
2018-03-05 10:40:20 +0000

    Adding variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 variable "aws_profile" {
   default = "default"
 }
-variable "server_port" {
-	description = "The port the servers will use for HTTP requests"
-	default = "8080"
-}
 variable "aws_region" {
   default = "us-east-1"
 }
@@ -12,3 +8,33 @@ variable "key_pair" {
   default = "deployer-key"
 }
 
+variable "aws_region" {}
+variable "aws_profile" {}
+data "aws_availability_zones" "available" {}
+variable "localip" {}
+variable "vpc_cidr" {}
+
+variable "cidrs" {
+  type = "map"
+}
+
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "domain_name" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
+variable "lc_instance_type" {}
+variable "delegation_set" {}
